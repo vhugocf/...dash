0a5ce62c8098f8d5cf75730907803a43cf4936af
victor hugo <vhugocf@hotmail.com>
2019-05-15 14:26:41 -0300

    commit1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import Piada from './Piada'
 
+// no div vamos renderizar os cinco react componentes
 function App() {
   return (
     <div>
-      
-         <h1>Victor Hugo</h1>
-      
-      
+      <Piada
+        pergunta="Qual é a melhor coisa sobre a Suiça?"
+        resposta="A bandeira é uma grande plus"
+      />
+
+      <Piada
+        pergunta="já ouviu falar do matematico que tem medo de numeros negativos?"
+        resposta="nunca ouvi falar"
+      />
+
+
+      <Piada pergunta="o que cai em pé e corre deitado?" resposta="a chuva" />
+      <Piada
+        pergunta="Qual será o seu REACT ao meu Currículo?"
+        resposta="vou me esforçar para te surpreender"
+
+
+      />
     </div>
   );
 }
---
+++ src/Piada.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Piada(props){
+    
+    return(
+        <div>
+        <h3>Pergunta: {props.pergunta}  </h3>
+        <h3>Resposta: {props.resposta} </h3>
+ 
+        </div>
+    )
+
+}
+
+
+
+
+
+export default Piada
+
